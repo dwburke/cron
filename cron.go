7062ec3a22e348f73de40d3243fcea8d5c4956c0
Dan Burke <dburke@addictmud.org>
2019-02-17 14:21:32 -0500

    initial commit

---
+++ cron.go
@@ -0,0 +1,36 @@
+package cron
+
+import (
+	"github.com/dwburke/atexit"
+	"github.com/robfig/cron"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+var Cron *cron.Cron
+
+func init() {
+	viper.SetDefault("cron.enabled", false)
+}
+
+func Run() {
+	if viper.GetBool("cron.enabled") == false {
+		log.Info("cron.enabled == false; not starting")
+		return
+	}
+
+	log.Info("cron.enabled == true; starting")
+
+	Cron = cron.New()
+
+	Cron.Start()
+
+	atexit.Add(Shutdown)
+}
+
+func Shutdown() {
+	if Cron != nil {
+		log.Info("cron: shutting down")
+		Cron.Stop()
+	}
+}
